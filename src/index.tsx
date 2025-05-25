ece3ea1e55bc0c415feae05b655c9912f059a089
salauddin <salauddinn@users.noreply.github.com>
2025-05-25 12:39:35 +0530

    Set up React E-commerce Project

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import App from './App';
+import './index.css';
+import { Toaster } from 'sonner';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <Toaster position="top-center" />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
